5074c4ad58fe25c612073e7005327dbad6483306
Vessela Naydenova <vesela.naidenova777@gmail.com>
2021-04-15 18:14:13 +0300

    Create useRecipeAsyncActions & useAuthAsyncActions

---
+++ frontend/src/state/asyncActions/auth.js
@@ -0,0 +1,29 @@
+import {useContext} from 'react'
+import { StateContext } from '../context'
+import useService from '../../api/useService'
+
+const useAuthAsyncActions = () => {
+  const {setToken} = useContext(StateContext)
+
+  const {login: loginService, register: registerService} = useService()
+
+  const login = ( userName, password) => {
+    return loginService(userName, password)
+      .then(res => {
+        console.log({res})
+        setToken(res.token)
+      })
+  }
+
+  const register = userInfo => {
+    return registerService(userInfo)
+      .then(res => {
+        console.log({res})
+      })
+  }
+  return {
+    login,
+    register
+  }
+}
+export default useAuthAsyncActions
---
+++ frontend/src/state/asyncActions/recipe.js
@@ -0,0 +1,68 @@
+import { success, request, failure, update, delAction, insert} from '../actions/creators'
+import { StateContext } from '../context'
+import { useCallback, useContext } from 'react'
+import useService from '../../api/useService'
+
+const useRecipeAsyncActions = () => {
+	const { dispatchRecipe: dispatch } = useContext(StateContext)
+	const {getRecipes, addRecipe: insertRecipe, deleteRecipe, editRecipe } = useService()
+
+	const loadRecipes = useCallback(() => {
+		dispatch(request())
+
+		return getRecipes()
+			// .then((recipes) => setRecipes({
+			// 	isPending: false,
+			// 	error: '',
+			// 	recipes
+			// }))
+			.then((recipes) => dispatch(success(recipes)))
+			.catch((error) =>	dispatch(failure(error.message)))
+	}, [dispatch])
+
+  const modifyRecipe = (fn, action) => recipe => {
+		dispatch(request())
+
+		return fn(recipe)
+			.then(res => {
+				if (res.status === 'error') {
+					dispatch(failure(res.message))
+          return res
+				}
+
+				dispatch(action(recipe))
+
+				return res
+			})
+			.catch(error => dispatch(failure(error.message)))
+	}
+	const addRecipe = modifyRecipe(insertRecipe, insert)
+	const updateRecipe = modifyRecipe(editRecipe, update)
+	const removeRecipe = modifyRecipe(deleteRecipe, delAction)
+
+	// const removeRecipe = id => {
+	// 	dispatch(request())
+	//
+	// 	return deleteRecipe(id)
+	// 		.then(res => {
+	// 			if (res.status === 'error') {
+	// 				dispatch(failure(res.message))
+	// 				return res
+	// 			}
+	//
+	// 			dispatch(delAction(id))
+	//
+	// 			return res
+	// 		})
+	// 		.catch(error => dispatch(failure(error.message)))
+	// }
+
+	return {
+		loadRecipes,
+		addRecipe,
+    updateRecipe,
+		removeRecipe
+	}
+}
+
+export default useRecipeAsyncActions
