6877ab1b3a6680dd238c0655194537392403f3f3
Vessela Naydenova <vesela.naidenova777@gmail.com>
2021-04-15 23:30:34 +0300

    Add validations to Login form

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,17 +1,45 @@
 import styles from './Login.module.css';
-import { useState } from 'react';
+import FormError from '../FormError/FormError';
+import { useCallback, useState } from 'react';
 import useAuthAsyncActions from '../../state/asyncActions/auth';
+import { validations } from '../../common/helpers';
+
+const required = (field) => (val) => {
+    if (val.length === 0) return `${field} is required!`;
+    return true;
+};
+
+const validationFns = {
+    userName: [required('User name')],
+    password: [required('Password')],
+};
 
 const Login = ({ history }) => {
     const [error, setError] = useState('');
+    const [validationErrors, setValidationErrors] = useState({});
+
     const { login } = useAuthAsyncActions();
+
+    const errorsForField = useCallback(
+        (field) => {
+            return validationErrors[field] || [];
+        },
+        [validationErrors]
+    );
+
     const handleSubmit = (e) => {
         e.preventDefault();
         const { userName, password } = e.target;
+        
 
-        login(userName.value, password.value)
-            .then(() => history.push('/'))
-            .catch((err) => setError(err.message));
+        const errors = validations(validationFns, {userName: userName.value, password: password.value});
+        // console.log({ errors });
+        setValidationErrors(errors);
+
+        Object.keys(errors).length === 0 &&
+            login(userName.value, password.value)
+                .then(() => history.push('/'))
+                .catch((err) => setError(err.message));
     };
 
     return (
@@ -25,6 +53,7 @@ const Login = ({ history }) => {
                     name='userName'
                     placeholder='Your username'
                 />
+                <FormError errors={errorsForField('userName')} />
                 <label htmlFor='password'>Password</label>
                 <input
                     type='password'
@@ -32,6 +61,7 @@ const Login = ({ history }) => {
                     name='password'
                     placeholder='Your password'
                 />
+                <FormError errors={errorsForField('password')} />
                 <button>Login</button>
             </form>
             {error && <div>error</div>}
